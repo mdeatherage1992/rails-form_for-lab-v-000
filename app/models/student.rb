277da8b41b9380ead5b36dbaf76eaefc35d9c339
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-03 13:49:30 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,33 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  def index
+  @students = Student.all
+  end
+
+def show
+  @student = Student.find(params[:id])
+end
+
+def new
+  @student = Student.new
+end
+
+def create
+  @student = Student.new
+  @Student.first_name = params[:first_name]
+  @student.last_name = params[:last_name]
+  @student.save
+  redirect_to student_path(@student)
+end
+
+def edit
+  @student = Student.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  @post.update(params.require(:post))
+  redirect_to post_path(@post)
+end
 end
