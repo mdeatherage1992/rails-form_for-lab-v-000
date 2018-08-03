9ab7e8b3dcea6d8b9417748717f4dfd43dfda4e0
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-03 14:15:10 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,7 +12,7 @@ def new
 end
 
 def create
-  @school_class = SchoolClass.new(params.requrie(:school_class))
+  @school_class = SchoolClass.new(params.require(:school_class))
   @school_class.save
   redirect_to school_class_path(@school_class)
 end
