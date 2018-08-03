f847f5c3de2ea577f8706bfb617c3e70cfa62ad3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-03 14:12:26 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,9 +12,7 @@ def new
 end
 
 def create
-  @student = Student.new
-  @student.first_name = params[:first_name]
-  @student.last_name = params[:last_name]
+  @student = Student.new(params.require(:student))
   @student.save
   redirect_to student_path(@student)
 end
